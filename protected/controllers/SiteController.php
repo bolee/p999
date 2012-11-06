1dc6e9ee4cd10e5647d2f4d936b1cb99882b0561
xingjia luo <bbsd@vip.qq.com>
2012-11-06 22:40:50 +0800

    修正大小写错误

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -30,7 +30,7 @@ class SiteController extends Controller
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
         /*查找问题*/
-        $question = new CActiveDataProvider('question',array(
+        $question = new CActiveDataProvider('Question',array(
             'criteria'=>array(
                 'order'=>'id DESC',
             ),
