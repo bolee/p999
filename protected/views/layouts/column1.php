9219021823b4a99dc8e86ad03ed165f46e383348
xingjia luo <bbsd@vip.qq.com>
2012-11-09 23:52:47 +0800

    统计代码

--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -64,6 +64,16 @@
         </div>
     </div>
 <!--补充end-->
+<div style="display:none">
+    <!-- Histats.com  START  (standard)-->
+    <script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+    <a href="http://www.histats.com" target="_blank" title="counter create hit" ><script  type="text/javascript" >
+        try {Histats.start(1,2119145,4,511,95,18,"00000000");
+            Histats.track_hits();} catch(err){};
+    </script></a>
+    <noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?2119145&101" alt="counter create hit" border="0"></a></noscript>
+    <!-- Histats.com  END  -->
+</div>
 </body>
 </html>
 <script type="text/javascript" src="./js/d.js"></script>
