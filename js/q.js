d5a81332a3503c0227f8910198ddda639001a1ac
qq277049 <bbsd@vip.qq.com>
2013-04-20 00:40:32 +0800

    bug fix

--- js/q.js
+++ js/q.js
@@ -1,13 +1,3 @@
 jQuery(function($){
-    var rHtml = function(t){
-        try{
-            $(t).html($(t).html().replace(/<(.*?)>/g,"&lt;$1&gt;"))
-        } catch (e) {
-        }
-    }
-    $.each($(".question-list h3"),function(i,v){
-        rHtml($(v).find('a'))
-    })
-    rHtml($('.post-title'))
 
 })
