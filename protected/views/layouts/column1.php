0b99e4d1bdc4f12d826789c744d5f3b36c85bc27
qq277049 <bbsd@vip.qq.com>
2013-04-19 21:20:42 +0800

    添加统计代码

--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -41,5 +41,23 @@
 
             <?php echo $content ?>
     </div>
+</div>
+<div class="modal-footer">
+    <!-- Start of StatCounter Code for Default Guide -->
+    <script type="text/javascript">
+        var sc_project=8883611;
+        var sc_invisible=0;
+        var sc_security="b38cd1fb";
+        var scJsHost = (("https:" == document.location.protocol) ?
+            "https://secure." : "http://www.");
+        document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+            "statcounter.com/counter/counter.js'></"+"script>");
+    </script>
+    <noscript><div class="statcounter"><a title="free hit counter"
+                                          href="http://statcounter.com/free-hit-counter/" target="_blank"><img
+                    class="statcounter" src="http://c.statcounter.com/8883611/0/b38cd1fb/0/"
+                    alt="free hit counter"></a></div></noscript>
+    <!-- End of StatCounter Code for Default Guide -->
+</div>
 </body>
 </html>
